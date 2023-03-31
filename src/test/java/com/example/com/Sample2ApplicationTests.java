4c6986518bb2a10aa82cd37e8eec51757964008c
rhwofid <rhwofid38@naver.com>
2023-03-31 11:21:19 +0900

    최초등록

---
+++ src/test/java/com/example/com/Sample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sample2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
